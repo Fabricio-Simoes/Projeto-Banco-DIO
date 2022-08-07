d22b423911a1be810d489540af908b755b6fbf78
Fabricio Simoes <fabriciofsf11@hotmail.com>
2022-08-06 23:01:51 -0300

    Projeto-DIO-Banco

---
+++ src/banco/Main.java
@@ -0,0 +1,19 @@
+package banco;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente fabricio = new Cliente();
+		fabricio.setNome("Fabricio");
+		
+		Conta cc = new ContaCorrente(fabricio);
+		Conta poupanca = new ContaPoupanca(fabricio);
+		
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
